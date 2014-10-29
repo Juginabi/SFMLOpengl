03414d7e0da99cdf32797f8ab237663f6a69ba8a
Jukka Vatjus-Anttila <jukka.vatjus-anttila@cyberlightning.com>
2014-10-29 12:05:11 +0200

    Fixed fatal bug found by Kari Vatjus-Anttila, developer of kNet wireshark plugin and RealXtend interest manager plugin and lead developer of legendary Mechforce project

--- SFMLopengl/main.cpp
+++ SFMLopengl/main.cpp
@@ -12,17 +12,25 @@ int main(int args, char* argv[]) {
 
 	// Clock for timing 'gameloop'
 	sf::Clock clock;
+	sf::Time frameStart;
+	sf::Time frameBudget = sf::milliseconds(16);
 
 	// Get renderer instance
 	renderer* renderer_ = renderer::getInstance();
 
+	clock.restart();
 	while (renderer_->isWindowOpen()) {
+		frameStart = clock.getElapsedTime();
 		// Update renderer. Events, drawing, etc.
-		renderer_->update(clock.getElapsedTime());
+		renderer_->update(frameStart);
 
 		// Sleep for rest of the 16ms left.
-		sf::sleep(sf::milliseconds(16-clock.getElapsedTime().asMilliseconds()));
+		sf::Time sleepDuration = frameBudget - (frameStart - clock.getElapsedTime());
+		std::cout << "Sleeping for " << sleepDuration.asMilliseconds() << " - frameTime: " << frameStart.asMilliseconds() <<  std::endl;
+		sf::sleep(sleepDuration);
 	}
+
+	std::cin.get();
 	
 	return EXIT_SUCCESS;
 
